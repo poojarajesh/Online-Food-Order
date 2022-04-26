a074415cd7d03ce98d7066180999d352260921c7
poojarajesh <poojarajesh189@gmail.com>
2022-04-26 22:47:17 +0530

    First Committ

---
+++ aboutus.php
@@ -0,0 +1,160 @@
+<?php
+session_start();
+?>
+
+<html>
+  <head>
+    <title> ABOUT US </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/aboutus.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Two Wheel Meal</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart 
+            (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+            </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <!-- <li> <a href="managersignup.php"> Manager Sign-up</a></li> -->
+          
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <!-- <li> <a href="managerlogin.php"> Manager Login</a></li> -->
+
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+        </div>
+
+      </div>
+    </nav>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+<style>
+body, html {
+  height: 100%;
+  font-family: "Inconsolata", sans-serif;
+}
+
+.bgimg {
+  background-position: center;
+  min-height: 50%;
+}
+
+.menu {
+  display: none;
+}
+</style> 
+<!-- Header with image -->
+ <header class="bgimg w3-display-container w3-grayscale-min" id="home">
+  <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small"></div>
+  <div class="w3-display-middle w3-center">
+    <span class="w3-text-black" style="font-size:90px">Two Wheel Meal</span>
+  </div>
+  <div class="w3-display-bottomright w3-center w3-padding-large"></div>
+</header> 
+
+<!-- Add a background color and large text to the whole page -->
+ <div class="w3-sand w3-grayscale w3-large"> 
+
+<!-- About Container -->
+<div class="w3-container" id="about">
+  <div class="w3-content" style="max-width:700px">
+    <h5 class="w3-center w3-padding-64"><span class="w3-tag w3-wide">ABOUT US</span></h5>
+    <p>We, here at Two Wheel Meal aim to bring you a collection of food items which would not only be fit for everyday but every mood of yours! We provide you with an array of choices that you would never feel anything missing from your life!</p>
+    <p>Customer satisfaction is our key goal. We constantly aim at imrpoving outnperformance inorder to see happy smiles on our customer's faces and we strive towards perfection but not only providing good food choices but also keeping in mind the best hygiene practices and ensuring that fresh and hot food is delivered to our customer's doorsteps!</p> 
+    <p>Are you ready to experience food heaven? Place your order now!</p>
+  </div> 
+</div> 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,212 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> Home Page</title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+
+  <link rel="stylesheet" type = "text/css" href ="css/index.css">
+
+  <body>
+
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Two Wheel Meal</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active" ><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart
+              (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+             </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <!-- <li> <a href="managersignup.php"> Manager Sign-up</a></li> -->
+              
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <!-- <li> <a href="managerlogin.php"> Manager Login</a></li> -->
+             
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+       </div>
+
+      </div>
+    </nav>
+
+    <!-- <div class="wide">
+      	<div class="col-xs-5 line"><hr></div>
+        <div class="col-xs-2 logo"><img src="images/Logo.jpg"></div>
+        <div class="col-xs-5 line"><hr></div>
+        <div class="tagline">Good Food is Good Mood</div>
+    </div> -->
+<!--
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+body {font-family: "Times New Roman", Georgia, Serif;}
+h1, h2, h3, h4, h5, h6 {
+  font-family: "Playfair Display";
+  letter-spacing: 5px;
+}
+</style>
+</head> -->
+
+<!-- Navbar (sit on top) -->
+<!--
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+    <a href="#home" class="w3-bar-item w3-button">Two Wheel Meal</a>
+    <div class="w3-right w3-hide-small">
+      <a href="#about" class="w3-bar-item w3-button">About</a>
+      <a href="#menu" class="w3-bar-item w3-button">Menu</a>
+      <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+    </div>
+  </div>
+</div>
+
+<header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px" id="home">
+  <img class="w3-image" src="/w3images/hamburger.jpg" alt="Hamburger Catering" width="1600" height="800">
+  <div class="w3-display-bottomleft w3-padding-large w3-opacity">
+    <h1 class="w3-xxlarge">Two Wheel Meal</h1>
+  </div>
+</header> -->
+<!--
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2{font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+p {line-height: 2}
+.bgimg {
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+.bgimg {background-image: url("/images/homepage.jpg")}
+</style> -->
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+<style>
+body, html {
+  height: 100%;
+  font-family: "Inconsolata", sans-serif;
+}
+
+.bgimg {
+  background-position: center;
+  min-height: 50%;
+}
+
+.menu {
+  display: none;
+}
+</style> 
+<!-- Header with image -->
+ <header class="bgimg w3-display-container w3-grayscale-min" id="home">
+  <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small"></div>
+  <div class="w3-display-middle w3-center">
+    <span class="w3-text-black" style="font-size:100px">Two Wheel Meal</span>
+  </div>
+  <div class="w3-display-bottomright w3-center w3-padding-large"></div>
+</header> 
+
+<!-- Add a background color and large text to the whole page -->
+ <div class="w3-sand w3-grayscale w3-large"> 
+
+<!-- About Container -->
+<div class="w3-container" id="about">
+  <div class="w3-content" style="max-width:900px">
+    <h2 class="w3-center w3-padding-64"><span class="w3-tag w3-wide">Feeling Hungry?</span></h2>
+  <!-- <div class="orderblock"> -->
+    <center><a class="btn btn-success btn-lg" href="customerlogin.php" role="button"> Order Now </a></center>
+    </div> 
+  
+</body>
+</html>
